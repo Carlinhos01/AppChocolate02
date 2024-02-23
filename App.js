a3e90866f2ffd9627f8f166a829cb8a57c118beb
Carlinhos01 <carloseduardoce47437@gmail.com>
2024-02-22 22:26:01 -0300

    App Chocolate02

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+      <Text style={styles.titulo}>Os Melhores Chocolates</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity>
+      <View style={styles.blocos}>
+          <Image style={styles.choco}
+          source={require('./assets/lacreme.png')}/>
+      </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+      <View style={styles.blocos}>
+          <Image style={styles.choco}
+          source={require('./assets/copenhague.png')}/>
+      </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+      <View style={styles.blocos}>
+          <Image style={styles.choco}
+          source={require('./assets/lollo.png')}/>
+      </View>
+      </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +36,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7b3614',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:
+  {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color:'#470b0a',
+    marginVertical: 40,
+  },
+  blocos:
+  {
+    height: 400,
+    backgroundColor:'#271618',
+    borderRadius: 50,
+    alignItems:'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  choco:
+  {
+    width: 350,
+    height: 300,
+  }
 });
---
+++ componentes/Rotas.js
@@ -0,0 +1,16 @@
+import {createStackNavigator} from '@react-nat=vigation/stack';
+
+import Lacreme from './Lacreme';
+import Copenhague from './Copenhague';
+import Lollo from './Lollo';
+
+const Stack = createStackNavigator();
+
+expor default function Rotas(){
+    return
+    (
+        <Stack.Navigator>
+            
+        </Stack.Navigator>
+    );
+}
